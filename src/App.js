41a1c3ae462563226c406fea36acef4d3c941cab
Rizwan Memon <rizwan2000.rm@gmail.com>
2020-06-10 07:02:03 +0530

    Comments in App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+// react
 import React from "react";
+// react-router-dom
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// css
 import "./App.css";
 
+// NAVBAR Component
 import Header from "./components/header/header.component";
+// LANDING Page
 import HomePage from "./pages/homepage/homepage.component";
+// SHOP Page
 import ShopPage from "./pages/shop/shop.component";
+// AUTHENTICATION Page
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+// FIREBASE
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
+// ^ MAIN CLASS COMPONENT
 class App extends React.Component {
-  constructor(props) {
+  // ! pass props in constructor if you ever need to use props in state
+  constructor() {
     super();
 
+    // ! APP COMPONENT STATE
     this.state = {
       currentUser: null
     };
@@ -19,11 +30,16 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
+  // * ONE OF THE LIFECYCLE METHOD IN REACT.COMPONENT
   componentDidMount() {
+    // * FIREBASE AUTHENTICATION
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      // this.setState({ currentUser: user });
-      // createUserProfileDocument(user);
-      // console.log(user);
+      // // this.setState({ currentUser: user });
+      // // createUserProfileDocument(user);
+      // // console.log(user);
+      // ^ IF USER IS FOUND SET STATE FOR currentUser
+
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -36,18 +52,23 @@ class App extends React.Component {
                 ...snapShot.data()
               }
             }
-            // () => console.log(this.state)
+            // //  () => console.log(this.state)
           );
         });
       }
+
+      // ^ ELSE SET STATE FOR currentUser to NULL
+
       this.setState({ currentUser: userAuth });
     });
   }
 
+  // * ONE OF THE LIFECYCLE METHOD IN REACT.COMPONENT
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
+  // EVERYTHING TO BE RENDERED ON PAGE GOES HERE
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+// ! REACT DOM takes care of best way to update DOM when react states are changes
 import ReactDOM from "react-dom";
-// import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
+// * RENDERING OUR MAIN APP COMPONENT IN INDEX.HTML PAGE
+// ! ALL OUR CODE IS WITHIN THIS MAIN APP COMPONENT
 ReactDOM.render(
   <React.StrictMode>
     <App />
