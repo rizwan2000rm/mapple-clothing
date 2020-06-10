8547d4c8b261cd823ad803b889ef5e50023ef2a7
Rizwan Memon <rizwan2000.rm@gmail.com>
2020-06-10 07:02:03 +0530

    Added cart quantity with redux

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,11 @@
 import CartActionTypes from "./cart.types";
 
+import { addItemToCart } from "./cart.utils";
+import { addItem } from "./cart.actions";
+
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +15,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
+
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+
     default:
       return state;
   }
