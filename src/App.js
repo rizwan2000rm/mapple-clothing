4d159473b0bbac35ec33f43b41bad183a758793a
Rizwan Memon <rizwan2000.rm@gmail.com>
2020-06-10 07:02:03 +0530

    updating our app with shop page, collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "./App.css";
-import HomePage from "./pages/homepage.component";
+
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </Router>
+      {/* <HomePage /> */}
     </div>
   );
 }
