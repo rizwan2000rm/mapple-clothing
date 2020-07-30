6cf82e2c1affc944558b2ba63a52f38d39da1c12
Rizwan Memon <rizwan2000.rm@gmail.com>
2020-07-30 18:59:42 +0530

    New Header Component, Added Materialize CSS

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,42 +1,130 @@
 import React from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import { auth } from "../../firebase/firebase.utils";
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../card-dropdown/card-dropdown.component";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 
-import {
-  HeaderContainer,
-  LogoContainer,
-  OptionsContainer,
-  OptionLink
-} from "./header.styles.jsx";
+import "./header.styles.scss";
 
-const Header = ({ currentUser, hidden }) => (
-  <HeaderContainer>
-    <LogoContainer to="/">
-      <Logo className="logo" />
-    </LogoContainer>
-    <OptionsContainer>
-      <OptionLink to="/shop">SHOP</OptionLink>
-      <OptionLink to="/shop">CONTACT</OptionLink>
-      {currentUser ? (
-        <OptionLink as="div" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </OptionLink>
-      ) : (
-        <OptionLink to="/signin">SIGN IN</OptionLink>
-      )}
-      <CartIcon />
-    </OptionsContainer>
-    {hidden ? null : <CartDropdown />}
-  </HeaderContainer>
-);
+const Header = ({ currentUser, hidden }) => {
+  useEffect(() => {
+    const M = window.M;
+
+    document.addEventListener("DOMContentLoaded", function () {
+      const elems = document.querySelectorAll(".sidenav");
+      // eslint-disable-next-line
+      const instances = M.Sidenav.init(elems, {
+        draggable: true,
+        edge: "right"
+      });
+    });
+  }, []);
+
+  return (
+    <header>
+      <div className="navbar-fixed">
+        <nav>
+          <div class="nav-wrapper teal">
+            <Link class="brand-logo" to="/">
+              Mapple Clothing
+            </Link>
+            <a href="/" data-target="slide-out" class="sidenav-trigger right">
+              <i class="material-icons">menu</i>
+            </a>
+
+            <ul class="right hide-on-med-and-down">
+              <li>
+                <Link to="/shop">
+                  <i className="material-icons left">store</i>
+                  SHOP
+                </Link>
+              </li>
+
+              <li>
+                {currentUser ? (
+                  <Link className="option click" onClick={() => auth.signOut()}>
+                    <i className="material-icons left">login</i>
+                    SIGN OUT
+                  </Link>
+                ) : (
+                  <Link className="option" to="/signin">
+                    <i className="material-icons left">login</i>
+                    LOGIN
+                  </Link>
+                )}
+              </li>
+              <li className="cart">
+                <CartIcon />
+              </li>
+              {hidden ? null : <CartDropdown />}
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <ul id="slide-out" className="sidenav">
+        <li>
+          <div className="user-view">
+            <div className="background">
+              <img
+                class="materialboxed"
+                width="300"
+                alt="background"
+                src="https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+              />
+            </div>
+            <h3>
+              <img
+                className="circle"
+                alt="logo"
+                src="https://png.pngtree.com/template/20190927/ourlarge/pngtree-initials-letter-m-logo-vector-template-designs-image_311990.jpg"
+              />
+            </h3>
+            <a href="#name">
+              <span className="name">Mapple Clothing</span>
+            </a>
+            <a href="#email">
+              <span className="email">enquiries.mappleclothing@gmail.com</span>
+            </a>
+          </div>
+        </li>
+        <li>
+          <Link to="/shop">
+            <i className="material-icons left">store</i>
+            SHOP
+          </Link>
+        </li>
+
+        <li>
+          {currentUser ? (
+            <Link className="option click" onClick={() => auth.signOut()}>
+              <i className="material-icons left">login</i>
+              SIGN OUT
+            </Link>
+          ) : (
+            <Link className="option" to="/signin">
+              <i className="material-icons left">login</i>
+              LOGIN
+            </Link>
+          )}
+        </li>
+        <li className="cart">
+          <Link className="option" to="/checkout">
+            <i class="material-icons shopping-icon">shopping_cart</i>
+            CART
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
 
 const mapStateToProps = (state) =>
   createStructuredSelector({
