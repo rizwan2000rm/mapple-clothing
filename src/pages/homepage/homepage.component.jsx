fcc93ac462c5dc1ebdefdbaefd63f7be89e0d6cf
Rizwan Memon <rizwan2000.rm@gmail.com>
2020-07-31 12:55:27 +0530

    Removed Carousel

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-import Carousel from "../../components/carousel/carousel.component";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
   <div className="container">
-    <Carousel />
     <Directory />
   </div>
 );
